66d52ff194a67a12681edd54af4eafad9406d9d2
JonathanGRodriguez <jonathangrodriguezramirez@gmail.com>
2020-11-27 12:46:53 -0600

    Primer Commit

---
+++ WebApplication1/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult GetAll()
+        {
+
+            return View();
+        }
+        public ActionResult Pruebas()
+        {
+            return View();
+        }
+    }
+}
